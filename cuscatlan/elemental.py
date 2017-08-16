479ba9650dba2e60962cefb071a18bf0bfc3a30f
edjuaro <edjuaro@gmail.com>
2017-08-16 12:38:27 -0700

    Adding a proof of concept function (custom_pearson)

--- cuscatlan/elemental.py
+++ cuscatlan/elemental.py
@@ -1,2 +1,6 @@
 def hellopipilworld():
     return u'Niltze Cemanahuac! (Hello "World"! in Nahuatl.)'
+
+
+def custom_pearson(x, y):
+    return scipy.stats.pearsonr(x, y)[0]
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='cuscatlan',
-      version='0.0.2',
-      description="Edwin Juarez's personal library",
+      version='0.0.3',
+      description="Edwin Juarez's personal library. Version 0.0.3 adds one proof-of-concept function (custom_pearson).",
       url='https://github.com/edjuaro/cuscatlan',
       author='Edwin F. Juarez',
       author_email='ejuarez@ucsd.edu',
