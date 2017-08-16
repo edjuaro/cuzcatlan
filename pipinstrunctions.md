87c1884e006b0a19a2d7a75d685b1008d8f2563f
edjuaro <edjuaro@gmail.com>
2017-08-16 11:19:28 -0700

    adding a file for reference

---
+++ pipinstrunctions.md
@@ -0,0 +1,16 @@
+## To publish to PyPi use
+
+python setup.py sdist upload
+
+## The (partial) PyPi credentials are in
+~/.pypirc
+
+and it looks like this:
+[distutils]
+index-servers =
+    pypi
+
+[pypi]
+repository: https://pypi.python.org/pypi
+username:edjuaro
+password:
--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
 setup(name='cuscatlan',
-      version='0.0.1',
+      version='0.0.2',
       description="Edwin Juarez's personal library",
       url='https://github.com/edjuaro/cuscatlan',
       author='Edwin F. Juarez',
       author_email='ejuarez@ucsd.edu',
       license='MIT',
       packages=['cuscatlan'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[
+          'numpy',
+          'scipy',
+          'pandas',
+          'matplotlib',
+          ],
+      )
