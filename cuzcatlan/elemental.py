430b757e15df987b294f74b93e55826051ab2c9c
edjuaro <edjuaro@gmail.com>
2017-08-17 11:50:24 -0700

    adding a few more functions to match current version of HierarchicalClustering module on GenePattern (v6)

--- cuzcatlan/elemental.py
+++ cuzcatlan/elemental.py
@@ -10,6 +10,14 @@ def custom_pearson(x, y):
     return scipy.stats.pearsonr(x, y)[0]
 
 
+def custom_spearman(x, y):
+    return scipy.stats.spearmanr(x, y)[0]
+
+
+def custom_kendall_tau(x, y):
+    return scipy.stats.kendalltau(x, y)[0]
+
+
 def absolute_pearson(x, y):
     return np.abs(scipy.stats.pearsonr(x, y)[0])
 
@@ -25,3 +33,14 @@ def uncentered_pearson(x, y):
         # x_squared =
         # y_squared =
         return np.sum(np.multiply(x, y)) / (np.sqrt(np.sum(np.square(x))) * np.sqrt(np.sum(np.square(y))))
+
+
+def absolute_uncentered_pearson(x, y):
+    if len(x) != len(y):
+        # Uncentered Pearson Correlation cannot be computed for vectors of different length.
+        print('Uncentered Pearson Correlation cannot be computed for vectors of different length.')
+        return np.nan
+
+    else:
+        # Using the definition from eq (4) in https://www.biomedcentral.com/content/supplementary/1477-5956-9-30-S4.PDF
+        return np.abs(np.sum(np.multiply(x, y)) / (np.sqrt(np.sum(np.square(x))) * np.sqrt(np.sum(np.square(y)))))
